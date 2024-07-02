e7c858846c14372411a351c90c795a7eea3bd40b
Dexter King <kingd2220@gmail.com>
2024-07-02 18:06:30 -0500

    Add UI for the game using Javascript.

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+let humanScore = 0;
+let computerScore = 0;
+
+const result = document.querySelector(".result");
+const score = document.querySelector(".score")
+
 function getComputerChoice() {
     let random = Math.random();
     if (random <= 0.33333) {
@@ -12,43 +18,56 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-   let choice = prompt("Rock, Paper, Scissors");
-   return choice;
+    let choice = prompt("Rock, Paper, Scissors");
+    return choice;
 }
 
 
-function playGame() {
 
-    function playRound(humanChoice, computerChoice) {
-        humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
-    
-        let loseMessage = "You lose! " + computerChoice + " beats " + humanChoice + "!";
-        let winMessage = "You win! " + humanChoice + " beats " + computerChoice + "!";
-    
-        if (humanChoice == computerChoice) {
-            return "Draw!";
+
+function playRound(humanChoice, computerChoice) {
+
+    humanChoice = humanChoice.charAt(0).toUpperCase() + humanChoice.slice(1).toLowerCase();
+
+    let loseMessage = "You lose! " + computerChoice + " beats " + humanChoice + "!";
+    let winMessage = "You win! " + humanChoice + " beats " + computerChoice + "!";
+
+    if (humanChoice == computerChoice) {
+        result.textContent = "Draw!";
+    }
+    else {
+        if (humanChoice == "Rock" && computerChoice == "Scissors" ||
+            humanChoice == "Paper" && computerChoice == "Rock" ||
+            humanChoice == "Scissors" && "Paper") {
+            humanScore++;
+            result.textContent = winMessage;
         }
         else {
-            if (humanChoice == "Rock" && computerChoice == "Scissors" || 
-            humanChoice == "Paper" && computerChoice == "Rock" || 
-            humanChoice == "Scissors" && "Paper") {
-                humanScore++;
-                console.log(winMessage);
-            }
-            else {
-                computerScore++;
-                console.log(loseMessage);
-            }
+            computerScore++;
+            result.textContent = loseMessage;
         }
     }
-
-    let humanScore = 0;
-    let computerScore = 0;
-
-    for (i = 1; i <= 5; i++) {
-        console.log(playRound(getHumanChoice(), getComputerChoice()));
-    }
-
-    console.log("Your final score is " + humanScore + ". The computer's score is " + computerScore + ".")
 }
 
+function resetScore() {
+    humanScore = 0;
+    computerScore = 0;
+}
+
+const button = document.querySelector('.buttonContainer');
+
+button.addEventListener("click", (event) => {
+    let target = event.target;
+
+    playRound(target.className, getComputerChoice());
+    score.textContent = "Your score is " + humanScore + ". The computer's score is " + computerScore + ".";
+    
+    if (humanScore === 5) {
+        score.textContent += " You win the game!";
+        resetScore();
+    }
+    if (computerScore === 5) {
+        score.textContent += " You lost the game! Better luck next time!";
+        resetScore();
+    }
+});
